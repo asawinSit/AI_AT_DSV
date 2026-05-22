ae62de8a9978ec3ff1f1125f20c8baf98c6496b4
asawinSit <asawin.sitthi@outlook.com>
2026-05-22 21:27:19 +0200

    a

---
+++ Inlamningsuppgift_3/Source/Inlamningsuppgift_3/Inlamningsuppgift_3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Inlamningsuppgift_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Inlamningsuppgift_3, "Inlamningsuppgift_3" );
